15ad1285066bbaa9e3c88d47cfddc405b884ad73
Abdul Hadi Alaidi <abd_ul_hadi@outlook.com>
2016-02-18 02:34:06 +0300

    first init

---
+++ WebApplication1/Controllers/StudentController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        public ActionResult Index()
+        {
+            List<Student> model = new List<Student>();
+            for (int i = 0; i < 5; i++)
+            {
+                var student = new Student
+                {
+                    Id = i.ToString(),
+                    FullName = "Student " + i,
+                    StudentStatusList = new List<StudentStatus>
+                    {
+                        new StudentStatus {ID = "1", Type = "Present"},
+                        new StudentStatus {ID = "2", Type = "Absent"},
+                        new StudentStatus {ID = "3", Type = "Unrecorded"}
+                    }
+                };
+                model.Add(student);
+            }
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Index(FormCollection model)
+        {
+            if (ModelState.IsValid)
+            {
+                //TODO: Save your model and redirect
+            }
+            return View(model);
+        }
+    }
+}
---
+++ WebApplication1/Models/Student.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class Student
+    {
+        public string Id { get; set; }
+        [Display(Name = "Full Name ")]
+        public string FullName { get; set; }
+        [Display(Name = "Student Status")]
+        public int StudentStatus { get; set; }
+        public List<StudentStatus> StudentStatusList { get; set; }
+    }
+    public class StudentStatus
+    {
+        public string ID { get; set; }
+        public string Type { get; set; }
+    }
+}
